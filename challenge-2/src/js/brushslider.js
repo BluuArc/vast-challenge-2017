49e1c4b502d07f959a0e41075c7e4a3fab4c69fe
Joshua Castor <jcasto3@uic.edu>
2017-07-09 21:57:03 -0500

    Added more axis code

--- challenge-2/src/js/brushslider.js
+++ challenge-2/src/js/brushslider.js
@@ -12,6 +12,7 @@ let TimeSlider = function(options){
     let scales = {}, axes = {};;
     let lines = [];
     let verbose = options.verbose || false;
+    const chemical_names = ['Appluimonia', 'Chlorodinine', 'Methylosmolene', 'AGOC-3A'];
 
     //default selections
     let selectedSensor = 'sensor1';
@@ -61,7 +62,7 @@ let TimeSlider = function(options){
         scales.monthScales = monthScales;
 
         //take every chemical scale and convert them to delta scales, where all input is output to a range of -1 and 1
-        let chemical_names = ['Appluimonia', 'Chlorodinine', 'Methylosmolene', 'AGOC-3A'];
+        // let chemical_names = ['Appluimonia', 'Chlorodinine', 'Methylosmolene', 'AGOC-3A'];
         for(let c of chemical_names){
             let [min,max] = options.scales[c].domain();
             let globalMax = Math.max(Math.abs(max),Math.abs(min)); //get value furthest from 0 for uniform vertical scaling
@@ -72,42 +73,84 @@ let TimeSlider = function(options){
         //used for debugging positions
         scales.verticalScale = d3.scaleLinear().domain([-1,1]).range(svgRange.y);
 
+        //draw background
+        svg.append('rect').classed('slider-background',true)
+            .attr('width',w-padding*2).attr('height',h-padding*2)
+            .attr('x',padding).attr('y',padding);
+
+
+        drawAxes();
+        // drawBrush();
+    };
+
+    function drawAxes(){
+        let group = svg.append('g').classed('delta-axes',true);
+
         //create vertical axes
-        for(let c of chemical_names){
+        for (let c of chemical_names) {
             axes[c] = d3.axisLeft(scales[c]).ticks(3);
         }
 
         //draw 0 line
-        svg.append('path')
-            .datum([new Vector(padding,scales.verticalScale(0)),new Vector(w-padding,scales.verticalScale(0))])
-            .attr('d',line);
-        
-        self.changeChemical(selectedChemical);
+        group.append('path').classed('axis',true)
+            .datum([new Vector(padding, scales.verticalScale(0)), new Vector(w - padding, scales.verticalScale(0))])
+            .attr('d', line)
+            .attr('stroke','black');
+
+
+        //draw left axis
+        group.append('g')
+            .classed('axis', true)
+            .classed('delta-axis-y', true)
+            .attr('transform', `translate(${padding},0)`)
+            .call(axes[selectedChemical]);
 
         //create horizontal axes
-        for(let m in monthScales){
-            // let m = 'April 2016';
+        let monthScales = scales.monthScales;
+        for (let m in monthScales) {
             axes[m] = d3.axisBottom(monthScales[m])
                 .ticks(d3.timeWeek)
                 .tickPadding(0);
-            svg.append('g')
-                .classed('axis',true)
-                .classed('axis-grid',true)
-                .attr('transform',`translate(0,${h-padding})`)
+            //draw bottom axes
+            group.append('g')
+                .classed('axis', true)
+                // .classed('axis-grid',true)
+                .attr('transform', `translate(0,${h - padding})`)
                 .call(axes[m])
-                // .selectAll('tick')
+
+            //draw daily dividers
+            // group.append('g')
+            //     .classed('axis', true).classed('minor-grid', true)
+            //     .attr('transform', `translate(0,${h - padding})`)
+            //     .call(d3.axisBottom(monthScales[m])
+            //         .ticks(d3.timeDay)
+            //         .tickSize(-h + padding * 2)
+            //         .tickFormat(() => { return null; }))
+            //     .selectAll('.tick').classed('tick-minor', true);
+            //draw monthly dividers
+            //based on https://bl.ocks.org/mbostock/6232537
+            group.append('g')
+                .classed('axis', true).classed('grid', true)
+                .attr('transform', `translate(0,${h - padding})`)
+                .call(d3.axisBottom(monthScales[m])
+                    .ticks(d3.timeMonth)
+                    .tickSize(-h + padding * 2)
+                    .tickFormat(() => { return null; }));
+
+            //draw top line
+            group.append('g').classed('axis', true)
+                .attr('transform', `translate(0,${padding})`)
+                .call(d3.axisBottom(monthScales[m])
+                    .ticks(d3.timeMonth)
+                    .tickSize(0)
+                    .tickFormat(() => { return null; }));
+
         }
-        let monthDividers = [(w/3)-padding,(w/3)-padding,(2*w/3)-padding];
-        for(let m of monthDividers){
-            svg.append('path')
-                .classed('month-divider',true)
-                .datum([new Vector(m,padding),new Vector(m,h-padding)])
-                .attr('d',line);
-        }
-    };
+        
+    }
 
     self.changeChemical = (newChemical) => {
-        let chemical_names = ['Appluimonia', 'Chlorodinine', 'Methylosmolene', 'AGOC-3A'];
+        // let chemical_names = ['Appluimonia', 'Chlorodinine', 'Methylosmolene', 'AGOC-3A'];
         if(chemical_names.indexOf(newChemical) === -1){
             throw new Error(`Chemical ${newChemical} is not found.`);
         }else{
@@ -115,17 +158,14 @@ let TimeSlider = function(options){
         }
 
         console.log("Selected chemical is now",selectedChemical);
-        d3.selectAll('.delta-axis-y').remove();
 
-        //plot axis
-        let yAxis = svg.append('g')
-            .classed('axis', true)
-            .classed('delta-axis-y', true)
-            .attr('transform', `translate(${padding},0)`)
-            .call(axes[selectedChemical]);
     };
 
-    self.plotDeltas = (data) => {
+    //data should have 2 fields: chemical and delta
+    //chemical is a string with the chemical name
+    //sensor is a string with the sensor name
+    //delta should be in the same format as challenge2.data.delta
+    self.update = (data) => {
 
     };
 
